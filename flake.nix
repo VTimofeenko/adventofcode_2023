b99dd00912c844775fe4327514b717975fd9a7b0
Vladimir Timofeenko <id@vtimofeenko.com>
2023-12-13 16:18:48 -0800

    feat: d1 p1

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devshell.flakeModule ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devshells.default = {
+          env = [ ];
+          commands = [ ];
+          packages = [
+            pkgs.nickel
+            pkgs.nls
+          ];
+        };
+
+      };
+    };
+}
